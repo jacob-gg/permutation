0f8db63e734893f11c473d9325c45a44c6082d30
jacob-gg <jrg2xq@virginia.edu>
2020-12-03 00:08:14 -0500

    spacing fix

--- R/permutation_cor.R
+++ R/permutation_cor.R
@@ -25,48 +25,48 @@
 #' @export
 permutation_cor <- function(x, y, iterations = 10^4, method = 'pearson', seed = 4){
 
-if('lawstat' %in% installed.packages() == FALSE){
-  stop('Error: lawstat not installed; use install.packages("lawstat")')
-}
-if('ggplot2' %in% installed.packages() == FALSE){
-  stop('Error: ggplot2 not installed; use install.packages("ggplot2")')
-}
-
-set.seed(seed)
-
-observed_cor <- cor(x, y, method = method)
-null_cor_dist <- rep(NA, iterations)
-for (i in 1:iterations) {
-  index <- sample(length(x), replace = FALSE)
-  permuted_x_vals <- x[index]
-  null_cor_dist[i] <- cor(permuted_x_vals, y, method = method)
-}
-cat('The observed correlation is r =', observed_cor, '\n')
-p_less <- (sum(null_cor_dist <= observed_cor) + 1) / (length(null_cor_dist) + 1)
-p_greater <- (sum(null_cor_dist >= observed_cor) + 1) / (length(null_cor_dist) + 1)
-if (p_less < p_greater) {
-  p_val <- p_less*2
-} else {
-  p_val <- p_greater*2
-}
-cat('The two-sided p-value for the permutation test is', p_val, '\n')
-invisible(symmetric <- lawstat::symmetry.test(null_cor_dist, boot = FALSE))
-if (symmetric$p.value > .05) {
-  cat('The null distribution for the permutation test is symmetric, p =', round(symmetric$p.value, digits = 4), '\n')
-} else {
-  cat('The null distribution for the permutation test is NOT symmetric, p =', round(symmetric$p.value, digits = 4), '\n')
-  cat('Change the seed and run the test again')
-}
-
-suppressMessages(print(
-  ggplot2::ggplot(data.frame(null_cor_dist)) +
-    ggplot2::geom_histogram(ggplot2::aes(null_cor_dist)) +
-    ggplot2::geom_vline(xintercept = observed_cor, color = 'gold') +
-    ggplot2::labs(title = 'Null correlation distribution',
-                  subtitle = 'Gold line is observed correlation',
-                  x = 'Null distribution',
-                  y = 'Frequency') +
-    ggplot2::theme(plot.subtitle = ggplot2::element_text(face = 'italic'))
-))
+  if('lawstat' %in% installed.packages() == FALSE){
+    stop('Error: lawstat not installed; use install.packages("lawstat")')
+  }
+  if('ggplot2' %in% installed.packages() == FALSE){
+    stop('Error: ggplot2 not installed; use install.packages("ggplot2")')
+  }
+  
+  set.seed(seed)
+  
+  observed_cor <- cor(x, y, method = method)
+  null_cor_dist <- rep(NA, iterations)
+  for (i in 1:iterations) {
+    index <- sample(length(x), replace = FALSE)
+    permuted_x_vals <- x[index]
+    null_cor_dist[i] <- cor(permuted_x_vals, y, method = method)
+  }
+  cat('The observed correlation is r =', observed_cor, '\n')
+  p_less <- (sum(null_cor_dist <= observed_cor) + 1) / (length(null_cor_dist) + 1)
+  p_greater <- (sum(null_cor_dist >= observed_cor) + 1) / (length(null_cor_dist) + 1)
+  if (p_less < p_greater) {
+    p_val <- p_less*2
+  } else {
+    p_val <- p_greater*2
+  }
+  cat('The two-sided p-value for the permutation test is', p_val, '\n')
+  invisible(symmetric <- lawstat::symmetry.test(null_cor_dist, boot = FALSE))
+  if (symmetric$p.value > .05) {
+    cat('The null distribution for the permutation test is symmetric, p =', round(symmetric$p.value, digits = 4), '\n')
+  } else {
+    cat('The null distribution for the permutation test is NOT symmetric, p =', round(symmetric$p.value, digits = 4), '\n')
+    cat('Change the seed and run the test again')
+  }
+  
+  suppressMessages(print(
+    ggplot2::ggplot(data.frame(null_cor_dist)) +
+      ggplot2::geom_histogram(ggplot2::aes(null_cor_dist)) +
+      ggplot2::geom_vline(xintercept = observed_cor, color = 'gold') +
+      ggplot2::labs(title = 'Null correlation distribution',
+                    subtitle = 'Gold line is observed correlation',
+                    x = 'Null distribution',
+                    y = 'Frequency') +
+      ggplot2::theme(plot.subtitle = ggplot2::element_text(face = 'italic'))
+  ))
 
 }
