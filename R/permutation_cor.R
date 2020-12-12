f4f58edfc9d4e6e8a516e8380d14fe0dd1dcfcf4
jacob-gg <jrg2xq@virginia.edu>
2020-12-12 15:16:26 -0500

    add clock, up cor efficiency, half downsample groups balanced, set return values

--- R/permutation_cor.R
+++ R/permutation_cor.R
@@ -15,21 +15,21 @@
 #' @param iterations Number of permutations (default = 10000)
 #' @param method Correlation method (default = 'pearson')
 #' @param seed Set for reproducibility (default = 4)
-#' @return Results reported in the console; plot generated in the viewer
+#' @return Results reported in the console and a plot generated in the viewer. If \code{permutation_cor} is saved to an object, it will save, as a list, the observed correlation, two-sided p-value, number of permutations used, and correlation method.
 #' @examples
 #' set.seed(1969)
 #' x <- rnorm(1000, 0, 1)
 #' y <- x + rnorm(1000, 0, 30)
-#' x[sample(length(x), 5, replace = F)] <- NA
+#' x[sample(length(x), 5, replace = FALSE)] <- NA
 #' permutation_cor(x, y)
 #'
 #' @export
 permutation_cor <- function(x, y, iterations = 10^4, method = 'pearson', seed = 4){
   # Checks
-  if ('lawstat' %in% installed.packages() == FALSE) {
+  if ('lawstat' %in% utils::installed.packages() == FALSE) {
     stop('Error: lawstat not installed; use install.packages("lawstat")')
   }
-  if ('ggplot2' %in% installed.packages() == FALSE) {
+  if ('ggplot2' %in% utils::installed.packages() == FALSE) {
     stop('Error: ggplot2 not installed; use install.packages("ggplot2")')
   }
   if (length(x) != length(y)) {
@@ -41,24 +41,25 @@ permutation_cor <- function(x, y, iterations = 10^4, method = 'pearson', seed = 
   
   set.seed(seed)
   
-  # Structure data, omit NAs
+  # Omit NAs if present
   if (TRUE %in% unlist(lapply(list(x, y), is.na))) {
-    dat_complete <- na.omit(data.frame(x, y))
+    dat_complete <- stats::na.omit(data.frame(x, y))
     x <- dat_complete$x
     y <- dat_complete$y
     cat('Data are not pairwise complete: Observed correlation and null distribution calculated from ', length(x), 'pairwise-complete observations', '\n')
   }
   
   # Calculate observed correlation
-  observed_cor <- cor(x, y, method = method)
+  observed_cor <- stats::cor(x, y, method = method)
   
   # Permutation procedure
+  start <- Sys.time()
   null_cor_dist <- rep(NA, iterations)
   for (i in 1:iterations) {
-    index <- sample(length(x), replace = FALSE)
-    permuted_x_vals <- x[index]
-    null_cor_dist[i] <- cor(permuted_x_vals, y, method = method)
+    permuted_x_vals <- sample(x, size = length(x), replace = FALSE)
+    null_cor_dist[i] <- stats::cor(permuted_x_vals, y, method = method)
   }
+  fin <- Sys.time()
   
   # Calculate p-value: Proportion of permutations where abs(permuted correlation) > abs(observed correlation)
   cat('The observed correlation is r =', observed_cor, '\n')
@@ -71,8 +72,10 @@ permutation_cor <- function(x, y, iterations = 10^4, method = 'pearson', seed = 
     cat('The null distribution for the permutation test is symmetric, p =', round(symmetric$p.value, digits = 4), '\n')
   } else {
     cat('The null distribution for the permutation test is NOT symmetric, p =', round(symmetric$p.value, digits = 4), '\n')
-    cat('Change the seed and run the test again')
+    cat('Change the seed and run the test again', '\n')
   }
+  
+  cat('The permutation process took', round(fin - start, digits = 2), 'seconds to complete', '\n')
   
   # Histogram of null distribution
   suppressMessages(print(
@@ -85,5 +88,12 @@ permutation_cor <- function(x, y, iterations = 10^4, method = 'pearson', seed = 
                     y = 'Frequency') +
       ggplot2::theme(plot.subtitle = ggplot2::element_text(face = 'italic'))
   ))
+  
+  invisible(
+    list(observed_correlation = observed_cor,
+         two_sided_p_value = p_val,
+         permutations = iterations,
+         correlation_method = method)
+  )
   
 }
--- R/permutation_mdiff.R
+++ R/permutation_mdiff.R
@@ -9,29 +9,27 @@
 #'   \item Produce a histogram of the generated null distribution and overlay the observed mean difference on it (using \code{ggplot2})
 #' }
 #' Requires: \code{ggplot2} and \code{lawstat}
-#' 
-#' Note: If groups A and B are unbalanced, \code{permutation_mdiff}'s default is to generate permuted groups of sizes n_A and n_B; you can, however, use a downsampling procedure to generate permuted groups of size min(n_A, n_B); see above.
 #'
 #' @param groups The vector/data frame column containing the (two) group identifiers
 #' @param values The vector/data frame column containing the values to be compared (numeric or numeric-coercible)
 #' @param iterations Number of permutations (default = 10000)
 #' @param seed Set for reproducibility (default = 4)
-#' @param downsample If groups A and B are unbalanced, you can set the \code{downsample} argument to TRUE to use downsampling each permutation to generate permuted groups of size min(n_A, n_B); otherwise, \code{permutation_mdiff} will generate permuted groups of size n_A and n_B during the permutation procedure (default = FALSE)
-#' @return Results reported in the console; plot generated in the viewer
+#' @param downsample Note: If the two groups, A and B, are unbalanced, \code{permutation_mdiff}'s default is to generate permuted groups of sizes n_A and n_B; you can, however, set \code{downsample = T} to use a downsampling procedure each permutation to generate permuted groups of size min(n_A, n_B)
+#' @return Results reported in the console and a plot generated in the viewer. If \code{permutation_mdiff} is saved to an object, it will save, as a list, the observed difference, two-sided p-value, and number of permutations used.
 #' @examples
 #' set.seed(1969)
 #' groups <- c(rep('control', 500), rep('experimental', 495))
 #' values <- c(runif(500, 0, 5), runif(490, 0.25, 5), rep(NA, 5))
 #' permutation_mdiff(groups, values)
-#' permutation_mdiff(groups, values, downsample = T)
+#' permutation_mdiff(groups, values, downsample = TRUE)
 #'
 #' @export
-permutation_mdiff <- function(groups, values, iterations = 10^4, seed = 4, downsample = FALSE){
+permutation_mdiff <- function(groups, values, iterations = 10^4, seed = 4, downsample = FALSE) {
   # Checks
-  if ('lawstat' %in% installed.packages() == FALSE) {
+  if ('lawstat' %in% utils::installed.packages() == FALSE) {
     stop('Error: lawstat not installed; use install.packages("lawstat")')
   }
-  if ('ggplot2' %in% installed.packages() == FALSE) {
+  if ('ggplot2' %in% utils::installed.packages() == FALSE) {
     stop('Error: ggplot2 not installed; use install.packages("ggplot2")')
   }
   if (length(groups) != length(values)) {
@@ -48,7 +46,7 @@ permutation_mdiff <- function(groups, values, iterations = 10^4, seed = 4, downs
   test_data$groups <- as.factor(as.character(test_data$groups))
   test_data$values <- as.numeric(as.character(test_data$values))
   if (TRUE %in% is.na(test_data)) {
-    test_data <- na.omit(test_data)
+    test_data <- stats::na.omit(test_data)
     if (downsample == F) {
       cat('Data are incomplete: Observed mean difference and null distribution calculated from', nrow(test_data), 'complete observations', '\n')
     }
@@ -58,13 +56,13 @@ permutation_mdiff <- function(groups, values, iterations = 10^4, seed = 4, downs
   }
   
   # Calculate observed mean difference
-  mean_table <- aggregate(test_data$values, by = list(test_data$groups), FUN = mean)
+  mean_table <- stats::aggregate(test_data$values, by = list(test_data$groups), FUN = mean)
   colnames(mean_table) <- c('Group', 'Mean')
-  if(mean_table$Mean[1] > mean_table$Mean[2]){
-    observed_dif <- mean_table$Mean[1] - mean_table$Mean[2]
+  if (mean_table$Mean[1] > mean_table$Mean[2]) {
+    observed_diff <- mean_table$Mean[1] - mean_table$Mean[2]
     group_one_greater <- T
   } else {
-    observed_dif <- mean_table$Mean[2] - mean_table$Mean[1]
+    observed_diff <- mean_table$Mean[2] - mean_table$Mean[1]
     group_one_greater <- F
   }
   
@@ -77,6 +75,7 @@ permutation_mdiff <- function(groups, values, iterations = 10^4, seed = 4, downs
   
   # Permutation procedure without downsampling (default; i.e., creating permuted groups of size n_a and n_b)
   if (downsample == F) {
+    start <- Sys.time()
     for (i in 1:iterations) {
       permuted_test_data <- test_data
       permuted_test_data$values <- sample(permuted_test_data$values, size = nrow(permuted_test_data), replace = F)
@@ -87,14 +86,19 @@ permutation_mdiff <- function(groups, values, iterations = 10^4, seed = 4, downs
         null_mean_diff_dist[i] <- mean(permuted_test_data[permuted_test_data$groups == mean_table$Group[2], c('values')]) - mean(permuted_test_data[permuted_test_data$groups == mean_table$Group[1], c('values')]) 
       }
     }
+    fin <- Sys.time()
   }
   
   # Permutation procedure with downsampling (i.e., creating permuted groups of size min(n_a, n_b) each permutation)
   if (downsample == T) {
     n_per_group <- table(test_data$groups)
+    if (identical(n_per_group[[1]], n_per_group[[2]]) == T) {
+      stop('Downsampling requested despite groups being balanced (n in both = ', n_per_group[[1]], ') set downsample = F and run again')
+    }
     cat('Downsampling requested: Downsampling done each permutation to generate null-distribution groups of size n =', min(n_per_group), '\n')
     smaller_group_name <- names(which(n_per_group == min(n_per_group)))
     bigger_group_name <- names(which(n_per_group == max(n_per_group)))
+    start <- Sys.time()
     for (i in 1:iterations) {
       bigger_group_downsample <- sample(rownames(test_data[test_data$groups == bigger_group_name, ]), nrow(test_data[test_data$groups == smaller_group_name, ]), replace = F)
       smaller_group <- rownames(test_data[test_data$groups == smaller_group_name, ])
@@ -107,12 +111,13 @@ permutation_mdiff <- function(groups, values, iterations = 10^4, seed = 4, downs
         null_mean_diff_dist[i] <- mean(downsampled_permuted_test_data[downsampled_permuted_test_data$groups == mean_table$Group[2], c('values')]) - mean(downsampled_permuted_test_data[downsampled_permuted_test_data$groups == mean_table$Group[1], c('values')])
       }
     }
+    fin <- Sys.time()
   }
   
   # Calculate p-value: Proportion of permutations where abs(permuted mean difference) > abs(observed mean difference)
   print(mean_table, row.names = FALSE)
-  cat('The observed mean difference is ', observed_dif, '\n', sep = '')
-  p_val <- (sum(abs(null_mean_diff_dist) >= abs(observed_dif)) + 1) / (length(null_mean_diff_dist) + 1)
+  cat('The observed mean difference is ', observed_diff, '\n', sep = '')
+  p_val <- (sum(abs(null_mean_diff_dist) >= abs(observed_diff)) + 1) / (length(null_mean_diff_dist) + 1)
   cat('The two-sided p-value for the permutation test is', p_val, '\n')
   
   # Symmetry test
@@ -121,14 +126,16 @@ permutation_mdiff <- function(groups, values, iterations = 10^4, seed = 4, downs
     cat('The null distribution for the permutation test is symmetric, p =', round(symmetric$p.value, digits = 4), '\n')
   } else {
     cat('The null distribution for the permutation test is NOT symmetric, p =', round(symmetric$p.value, digits = 4), '\n')
-    cat('Change the seed and run the test again')
+    cat('Change the seed and run the test again', '\n')
   }
+  
+  cat('The permutation process took', round(fin - start, digits = 2), 'seconds to complete', '\n')
   
   # Histogram of null distribution
   suppressMessages(print(
     ggplot2::ggplot(data.frame(null_mean_diff_dist)) +
       ggplot2::geom_histogram(ggplot2::aes(null_mean_diff_dist)) +
-      ggplot2::geom_vline(xintercept = observed_dif, color = 'gold') +
+      ggplot2::geom_vline(xintercept = observed_diff, color = 'gold') +
       ggplot2::labs(title = 'Null mean-difference distribution',
                     subtitle = paste0('Gold line is observed mean difference, ', mean_table$Group[mean_table$Mean == max(mean_table$Mean)]
                                       , ' - ', mean_table$Group[mean_table$Mean == min(mean_table$Mean)]),
@@ -136,5 +143,11 @@ permutation_mdiff <- function(groups, values, iterations = 10^4, seed = 4, downs
                     y = 'Frequency') +
       ggplot2::theme(plot.subtitle = ggplot2::element_text(face = 'italic'))
   ))
+  
+  invisible(
+    list(observed_difference = observed_diff,
+         two_sided_p_value = p_val,
+         permutations = iterations)
+  )
   
 }
